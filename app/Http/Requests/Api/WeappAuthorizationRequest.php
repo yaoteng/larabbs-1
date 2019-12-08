896ba1572a77d4dcc944c601cdf3fd26c3445f1f
root <root@localhost.localdomain>
2019-12-08 19:38:53 +0800

    laravel weapp user store

---
+++ app/Http/Requests/Api/WeappAuthorizationRequest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+
+class WeappAuthorizationRequest extends FormRequest
+{
+
+    public function rules()
+    {
+        return [
+            'code' => 'required|string',
+            //
+        ];
+    }
+}
